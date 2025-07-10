b9c42de99c998c06f07760e5deebb1a95e73f0de
dotflux <venjirogaming@gmail.com>
2025-07-11 00:02:20 +0530

    Deploy site (hope so)

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="w-full border-t border-[#23243a] py-6 px-4 flex flex-col items-center justify-center text-center bg-transparent">
-      <span className="text-gray-400 text-sm">2025 All rights reserved.</span>
-    </footer>
-  );
-};
-
-export default Footer;
